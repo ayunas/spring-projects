d31425d4f7f9511776731dd5e853ad2262831e81
Amir Yunas <ayunas@gmail.com>
2020-10-01 15:48:15 -0400

    redirect in Thyme @Controller by returning a string : redirect:/urlPattern.  keep the data passed to the redirect using the RedirectAttributes attr parameter.  and addFlashAttribute()

--- spring-jobreadyprogrammer/spring-boot/src/main/java/com/jrp/springboot/web/ThymeController.java
+++ spring-jobreadyprogrammer/spring-boot/src/main/java/com/jrp/springboot/web/ThymeController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import com.jrp.springboot.domain.User;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class ThymeController {
@@ -16,7 +17,17 @@ public class ThymeController {
 	}
 
 	@RequestMapping(value="/create", method= RequestMethod.POST)
-	public String processThymeForm(User user) {
+	public String processThymeForm(User user, RedirectAttributes attr) {
+//		insert data submitted in the form into the database.
+		attr.addFlashAttribute("user",user);
+		return "redirect:/display-thyme";
+//		return "pages/thyme-process";
+	}
+
+	@RequestMapping(value="/display-thyme", method=RequestMethod.GET)
+	public String displayThymeFormData(User user) {
+		System.out.println(user.toString());
 		return "pages/thyme-process";
 	}
+
 }
